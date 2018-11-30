d39357912a61abc0b1b5db9fc4419c47ea0cbb78
lajawanti <myemail>
2018-11-30 12:11:42 -0500

    C:/tools/Git/api/login working

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,13 @@
+//require('./dotenv').config();
+
 const axios = require('axios');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const { authenticate } = require('./middlewares');
+
+//NEED jwtKey AS DEFINED IN AUTHENTICATE MIDDLEWARE WHILE GENERATING TOKEN..
+const jwtKey = require('../_secrets/keys').jwtKey;
 
 const db = require('../database/dbConfig.js');
 
@@ -12,10 +18,12 @@ module.exports = server => {
   server.get('/api/jokes', authenticate, getJokes);
 };
 
+//======= FUNCTION TO SEE SERVER RUNNING ON BASIC ROUTE '/' ========
 function serverRunning(req, res) {
     res.send('Server is ON');
 }
 
+//======= FUNCTION TO SEE REGISTER NEW USERE '/api/register' ========
 function register(req, res) {
     // implement user registration
     const credentials = req.body;
@@ -28,8 +36,36 @@ function register(req, res) {
                .catch(err => res.send(err));
 }
 
+//======= FOR LOGIN NEED GENERATE-TOKEN FUNCTION ========
+function generateToken(user) {
+    const jwtPayload = {
+        ...user,
+    };
+    //const jwtSecret = process.env.JWT_SECRET;
+
+    const jwtOptions = {
+        expiresIn : '1h',
+    };
+
+    return jwt.sign(jwtPayload, jwtKey, jwtOptions);
+}
+
+//======= FUNCTION LOGIN ========
 function login(req, res) {
-  // implement user login
+    // implement user login
+    const credentials = req.body;
+    db('users')
+          .where({ username : credentials.username })
+          .first()
+          .then(user => {
+              if(user && bcrypt.compareSync(credentials.password, user.password)) {
+                    const token = generateToken(user);
+                    res.status(200).json({message : "Logged In", token});
+              } else {
+                    res.status(401).json({message : "Invalid username or password.."})
+              }
+           })
+          .catch(err => res.send({Message : "Error in Logging In..."}));
 }
 
 function getJokes(req, res) {
